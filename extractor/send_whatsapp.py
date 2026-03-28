2f0eb475e20d28cff1ff0a5d7cf809d0e235d3de
Gabriel Ps <gps828523@gmail.com>
2026-03-28 15:41:07 +0530

    Update send_whatsapp.py

--- extractor/send_whatsapp.py
+++ extractor/send_whatsapp.py
@@ -42,7 +42,7 @@
 
 
 
-from selenium import webdriver
+# from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
